fece789a9b7864b547c4880e52728e96e3647e34
jlab001 <35016493+jlab001@users.noreply.github.com>
2018-11-21 15:46:06 +0800

    Update blockyTalkyBLE.ts

--- blockyTalkyBLE.ts
+++ blockyTalkyBLE.ts
@@ -48,38 +48,6 @@ namespace blockyTalkyBLE {
         handlers = newHandler;
     }
 
-    //% blockId=blockyTalkyBLE_send_string_key_value block="send string|key %key|value %value"
-    export function sendMessageWithStringValue(key: string, value: string): void {
-        sendRawMessage(key, ValueTypeIndicator.String, value)
-    }
-
-    //% blockId=blockyTalkyBLE_send_number_key_value block="send number|key %key|value %value"
-    export function sendMessageWithNumberValue(key: string, value: number): void {
-        sendRawMessage(key, ValueTypeIndicator.Number, value.toString())
-    }
-
-    function sendRawMessage(key: string, valueTypeIndicator: ValueTypeIndicator, value: string): void {
-        let indicatorAsString = getStringForValueTypeIndicator(valueTypeIndicator);
-        bluetooth.uartWriteString(indicatorAsString + delimiter + key + delimiter + value + terminator)
-    }
-
-    let splitString = (splitOnChar: string, input: string) => {
-      let result:string[] = []
-      let count = 0
-      let startIndex = 0
-      for (let index = 0; index < input.length; index++) {
-          if (input.charAt(index) == splitOnChar) {
-              result[count] = input.substr(startIndex, index - startIndex)
-
-              startIndex = index + 1
-              count = count + 1
-          }
-      }
-      result[count] = input.substr(startIndex, input.length - startIndex)
-
-      return result;
-    }
-
     /**
      * Get string representation of enum.
      */
